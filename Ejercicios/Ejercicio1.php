063b2481a7496a49ab94762bb0d9cbfc860a1f95
Danny0327 <juliethcamposbustos@gmail.com>
2022-05-04 11:00:18 -0500

    first commit

---
+++ Ejercicios/Ejercicio1.php
@@ -0,0 +1,14 @@
+<?php
+
+//  Instruccion de impresion de informacion
+/* 
+Comentario largo: 
+comentario de codigo
+*/
+echo "Hola my love 0";
+
+print_r("27"); 
+
+print("go go go");
+
+?>
---
+++ Ejercicios/ejercicio3.php
@@ -0,0 +1,15 @@
+<?php
+if($_GET){
+
+    $nombre=$_GET['nombre'];
+
+    echo "Hola ".$nombre;
+        
+        $b=$_GET['b'];
+
+        echo " Quiero agradecerte ".$b;
+
+        echo " Te amo ";
+}
+
+?>
